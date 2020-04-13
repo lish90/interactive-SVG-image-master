6e4e688be532a1d2965fb68bb07580545542573c
alisha musamabik <alisha.musamabik@gmail.com>
2020-04-13 21:08:46 +0100

    TDD challeneg -part 2: practice

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,15 +1,14 @@
-// import index from 'jest';
-const getSVG = require('../index.js');
+const getSVG = require('../index');
 
 test('load the svg', () => {
     let result = getSVG();
+    //expect(result).toBeTruthy();
     expect(result).toBeInstanceOf(SVGSVGElement);
 });
 
-
-// test('does something', () => {
-//     expect(true).toBe(true);
-//   });
-
-
-module.exports = getSVG;
+test('response to click events',() =>{
+    let svgOneState = getSVG().getElementById('Code_AK');
+    svgOneState.onClick = jest.fn();
+    let result =svgOneState.getElementsByTagName('title')[0].innerHTML;
+    expect(result).toBe('Alaska clicked')
+});
